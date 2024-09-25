1595b63296422f0d40e40335019f03198bb3d536
Hemanth <hemanthsiet05@gmail.com>
2024-09-25 15:42:10 +0530

    commit

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -11,10 +11,19 @@ const Navbar = () => {
             <img className="mx-2 h-7 "   src={logo} alt=""/>
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+            <a href="https://www.linkedin.com/in/hemanths5/" target="_blank">
             <FaLinkedin/>
+            </a>
+            <a href="https://github.com/Hemanths05" target="_blank">
             <FaGithub/>
+            </a>
+            <a href="https://www.hackerrank.com/profile/hemanthsiet05" target="_blank" >
             <FaHackerrank/>
-            <FaInstagram/>
+            </a>
+            <a href="https://leetcode.com/u/PRUZirrN7u/" target="_blank">
+            <img  width={24} height={24} src="https://leetcode.com/favicon.ico"
+             alt="LeetCode"/>
+            </a>
         </div>
     </nav>
   )
